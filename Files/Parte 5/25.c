f7c34668b59e42189aaf97faf3c9ee8553097603
Victor <vbhy91zr@duck.com>
2025-10-08 17:03:41 -0300

    add 25.c

--- Files/Parte 5/25.c
+++ Files/Parte 5/25.c
@@ -1,1 +1,41 @@
-2
+#include <string.h>
+#include <stdio.h>
+
+int main() {
+    FILE *file = fopen("texto.txt", "r+");
+    if (file == NULL) {
+        printf("Erro ao abrir arquivo.\n");
+        return 1;
+    }
+
+    char c;
+    int i = 0;
+    long pos;
+    char palavra[100];
+
+    while ((c = fgetc(file)) != EOF) {
+        if (c >= 'A' && c <= 'Z') c = c + ('a' - 'A');
+
+        if ((c >= 'a' && c <= 'z')) {
+            if (i == 0)
+                pos = ftell(file) - 1;
+            palavra[i++] = c;
+        } else {
+            if (i > 0) {
+                palavra[i] = '\0';
+                if (strcmp(palavra, "casa") == 0) {
+                    fseek(file, pos, SEEK_SET);
+                    fputs("lar", file);
+                    fputc(' ', file);
+                }
+                i = 0;
+            }
+
+            pos = ftell(file);
+        }
+    }
+
+
+    fclose(file);
+    return 0;
+}
